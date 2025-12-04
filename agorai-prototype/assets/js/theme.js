996f0e389e9314a26f1549096dc453cc229756e6
tantiiz <jarrenaceret2003@gmail.com>
2025-12-05 06:01:43 +0800

    2.1.0v: Dark mode

---
+++ agorai-prototype/assets/js/theme.js
@@ -0,0 +1,16 @@
+// Check for saved theme preference or system preference
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
+}
+
+function toggleTheme() {
+    if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark')
+        localStorage.theme = 'light'
+    } else {
+        document.documentElement.classList.add('dark')
+        localStorage.theme = 'dark'
+    }
+}
